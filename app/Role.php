1b7e8af903cc0b1205983b32311201a492624fd6
catalin bica <vagrant@ajutorspitale>
2020-03-30 17:04:11 +0000

    [DEV] - added role model + user role relation

---
+++ app/Role.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    protected $table = "metadata_user_role_types";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'label',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+}
