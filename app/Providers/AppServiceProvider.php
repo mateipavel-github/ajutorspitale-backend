c78ecd338bb2c6103f3c10cf8fd0f4f2e2993a1f
matei <matei+github@konk-media.com>
2020-03-31 13:37:50 +0000

    fix key length in password database table

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
 
+        Schema::defaultStringLength(191);
+
         if(env('DEBUG_SQL', false)) {
             \DB::listen(function ($query) {
                 \Log::info(
