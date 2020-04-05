1ba90334935eeefca406b4fade62d456c7d109e6
catalin bica <vagrant@ajutorspitale>
2020-04-05 12:55:07 +0000

    [IMPORT] - finished import + migration for 2 columns

--- app/Http/Controllers/Api/v1/ImportController.php
+++ app/Http/Controllers/Api/v1/ImportController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\HelpRequest;
 use App\Http\Controllers\Controller;
 
+use App\MetadataCounty;
+use App\MetadataMedicalUnitType;
 use App\MetadataNeedType;
+use App\MetadataRequestStatusType;
 use App\MetadataUserRoleType;
 use App\User;
+use Carbon\Carbon;
+use Doctrine\DBAL\Query\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -40,17 +46,36 @@ class ImportController extends Controller
 
     public function form_responses()
     {
+        set_time_limit(6000);
         $handle = fopen(resource_path('/csv/form_responses.csv'), 'r');
         $header = fgetcsv($handle);
         $volunteer_role = MetadataUserRoleType::where(['slug' => "volunteer"])->first();
+
         while (($data = fgetcsv($handle)) !== FALSE) {
-            $this->saveUser($data, $volunteer_role);
-            
+            $request_volunteer = $this->saveUser($data, $volunteer_role);
+            $existing_help_request = HelpRequest::where(['created_at' => Carbon::createFromFormat('m/d/Y H:i:s', $data[0])])->first();
+            if (empty($existing_help_request)) {
+                try {
+                    $this->saveHelpRequest($data, $request_volunteer);
+                } catch (\Exception $exception) {
+                    dd($header, $data, $exception);
+                }
+            }
+        }
+
+        //populating the other_needs
+        $handle = fopen(resource_path('/csv/date_publice.csv'), 'r');
+        $header = fgetcsv($handle);
+        while (($data = fgetcsv($handle)) !== FALSE) {
+            $existing_help_request = HelpRequest::where(['created_at' => Carbon::createFromFormat('m/d/Y H:i:s', $data[0])])->first();
+            $existing_help_request->other_needs = $data[21];
+            $existing_help_request->save();
         }
     }
 
     protected function saveUser($data, MetadataUserRoleType $volunteer_role)
     {
+        $existing_user = null;
         $users_on_multiple_lines = explode(PHP_EOL, $data[9]);
         foreach ($users_on_multiple_lines as $user_name) {
             $user_name = ucwords(strtolower(trim($user_name)));
@@ -68,5 +93,44 @@ class ImportController extends Controller
             }
 
         }
+        return $existing_user;
+    }
+
+    protected function saveHelpRequest($data, User $request_volunteer)
+    {
+        $status = null;
+        $medical_unit_type = MetadataMedicalUnitType::where(['label' => $data[7]])->first();
+
+        $county = MetadataCounty::where(['label' => ''])->first();
+        switch (strtolower(trim($data[11]))) {
+            case "da":
+                $status = MetadataRequestStatusType::where(['slug' => "approved"])->first();
+                break;
+            case "nu":
+                $status = MetadataRequestStatusType::where(['slug' => "rejected"])->first();
+                break;
+            case "rezolvat":
+                $status = MetadataRequestStatusType::where(['slug' => "complete"])->first();
+                break;
+            default:
+                $status = MetadataRequestStatusType::where(['slug' => "new"])->first();
+                break;
+        }
+        $help_request = new HelpRequest();
+        $help_request->user_id = null;
+        $help_request->assigned_user_id = $request_volunteer->id;
+        $help_request->name = ucwords(strtolower(trim($data[1])));
+        $help_request->phone_number = trim($data[2]);
+        $help_request->medical_unit_type_id = !empty($medical_unit_type) ? $medical_unit_type->id : null;
+        $help_request->medical_unit_name = trim($data[3]);
+        $help_request->job_title = trim($data[8]);
+        $help_request->extra_info = preg_replace('/[[:^print:]]/', '', trim($data[6]));
+        $help_request->needs_text = $data[4];
+        $help_request->status = $status->id;
+        $help_request->county_id = !empty($county) ? $county->id : null;
+        $help_request->caller_observations = $data[10];
+        $help_request->created_at = Carbon::createFromFormat('m/d/Y H:i:s', $data[0]);
+        $help_request->save();
+        return $help_request;
     }
 }
