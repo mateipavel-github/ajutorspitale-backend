e3c880ff2ccc982b88b301e6ac5aa4439ffb4255
Matei Pavel <matei+github@konk-media.com>
2020-04-11 00:32:03 +0300

    small fix

--- app/Http/Controllers/Api/v1/HelpOfferController.php
+++ app/Http/Controllers/Api/v1/HelpOfferController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Api\v1\PostingController;
 
 class HelpOfferController extends PostingController
 {
